0806410b4fccaaa95aa10afe12f2963bf471b80f
PatrykWojciechowski <patrickwww94@gmail.com>
2020-04-26 23:44:04 +0200

    Prettier clean up commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
