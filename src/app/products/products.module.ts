232d7a649ffc5f4be5bdc20d1993db1f5173635b
Patryk <patrickwww94@gmail.com>
2020-05-28 00:11:34 +0200

    Add feature store with schematics

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
+import { StoreModule } from '@ngrx/store';
+import * as fromProducts from './reducers';
 
 @NgModule({
   declarations: [ProductListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature(fromProducts.productsFeatureKey, fromProducts.reducers, { metaReducers: fromProducts.metaReducers })
   ]
 })
 export class ProductsModule { }
